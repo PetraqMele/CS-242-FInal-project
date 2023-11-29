609910b081a2bb70a9d7d1f262e0587025dfd2a0
PetraqMele <131476697+PetraqMele@users.noreply.github.com>
2023-11-29 14:36:49 +0000

    more planets

--- final_folder/main.java
+++ final_folder/main.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+
+        //Creating all the planets
+        Planet Sun = new Planet();
+        Planet Mercury = new Planet();
+        Planet Venus = new Planet();
         Planet Earth = new Planet();
+        Planet Mars = new Planet();
+        Planet Jupiter = new Planet();
+        Planet Saturn = new Planet();
+        Planet Uranus = new Planet();
+        Planet Neptune = new Planet();
+        
+        
+        
 	}
 
 }
