9f527c2798f9873d72a3f9e71a39b9936f9e9a36
PetraqMele <131476697+PetraqMele@users.noreply.github.com>
2023-12-13 01:23:56 +0000

    worked on the nodes (we coders fr)

---
+++ final_folder/Node.java
@@ -0,0 +1,45 @@
+package final_folder;
+
+public class Node {
+    //Here I am creating the node constructors and methods for the planet class.
+    public Planet planetData;//the data of the planet itself
+    public Node next;
+    public Node previous;
+
+        public Node(Planet planetData) {//base planet node
+           planetData = this.planetData;
+           next = null;
+           previous = null;
+        
+     }
+
+        public Node(Planet planetData, Node nextNode, Node previousNode){//planet node with previous and next link
+            planetData = this.planetData;
+            next = nextNode;
+            previous = previousNode; 
+        }
+
+        public Planet getPlanet(){
+            return planetData;
+        }
+
+        public void setPlanet(Planet newPlanet){
+            planetData = newPlanet;
+        }
+
+        public Node getNext(){
+            return next;
+        }
+
+        public Node getPrevious(){
+            return previous;
+        }
+
+        public void setNext(Node planet){
+            next = planet;
+        }
+
+        public void setPrevious(Node planet){
+            previous = planet;
+        }
+}
